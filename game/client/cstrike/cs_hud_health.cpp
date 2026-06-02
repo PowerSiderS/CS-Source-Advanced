cfd3b24febdc6e4cb20dc755d4277fb69284f536
PowerSiderS <nikoladark87@gmail.com>
2026-06-02 12:00:24 +0100

    Default Health & Armor Style

--- game/client/cstrike/cs_hud_health.cpp
+++ game/client/cstrike/cs_hud_health.cpp
@@ -14,7 +14,7 @@
 #include "hud.h"
 #include "hud_macros.h"
 #include "view.h"
-#include "cs_shareddefs.h"
+
 #include "iclientmode.h"
 
 #define PAIN_NAME "sprites/%d_pain.vmt"
@@ -32,8 +32,6 @@ using namespace vgui;
 #include "cs_hud_color.h"
 
 #include "convar.h"
-
-ConVar hud_healtharmor_style( "hud_healtharmor_style", "0", FCVAR_ARCHIVE, "0 = default, 1 = simple (old)", true, 0, true, HUD_STYLE_MAX );
 
 //-----------------------------------------------------------------------------
 // Purpose: Health panel
@@ -63,28 +61,7 @@ private:
         CPanelAnimationVarAliasType( float, icon_xpos, "icon_xpos", "0", "proportional_float" );
         CPanelAnimationVarAliasType( float, icon_ypos, "icon_ypos", "0", "proportional_float" );
 
-        CPanelAnimationVarAliasType( float, legacy_xpos, "legacy_xpos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, legacy_ypos, "legacy_ypos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, legacy_wide, "legacy_wide", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, legacy_tall, "legacy_tall", "0", "proportional_float" );
-
-        CPanelAnimationVarAliasType( float, simple_xpos, "simple_xpos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, simple_ypos, "simple_ypos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, simple_wide, "simple_wide", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, simple_tall, "simple_tall", "0", "proportional_float" );
-
-        CPanelAnimationVarAliasType( float, progress_xpos, "progress_xpos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, progress_ypos, "progress_ypos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, progress_wide, "progress_wide", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, progress_tall, "progress_tall", "0", "proportional_float" );
-        CPanelAnimationVar( Color, m_ProgressFgColor, "ProgressFgColor", "FgColor" );
-        CPanelAnimationVar( Color, m_ProgressBgColor, "ProgressBgColor", "BgColor" );
-
-        int m_iStyle;
-        int m_iOriginalXPos;
-        int m_iOriginalYPos;
-        int m_iOriginalWide;
-        int m_iOriginalTall;
+//      CPanelAnimationVar( Color, m_LowHealthColor, "LowHealthColor", "255 0 0 255" );
 
         float icon_tall;
         float icon_wide;
@@ -99,12 +76,6 @@ DECLARE_HUDELEMENT( CHudHealth );
 CHudHealth::CHudHealth( const char *pElementName ) : CHudElement( pElementName ), CHudNumericDisplay(NULL, "HudHealth"), m_pHealthIcon( NULL )
 {
         SetHiddenBits( HIDEHUD_HEALTH | HIDEHUD_PLAYERDEAD );
-
-        m_iStyle = -1;
-        m_iOriginalXPos = 0;
-        m_iOriginalYPos = 0;
-        m_iOriginalWide = 0;
-        m_iOriginalTall = 0;
 }
 
 //-----------------------------------------------------------------------------
@@ -136,8 +107,6 @@ void CHudHealth::ApplySchemeSettings( IScheme *scheme )
                 float scale = icon_tall / (float)m_pHealthIcon->Height();
                 icon_wide = ( scale ) * (float)m_pHealthIcon->Width();
         }
-
-        GetBounds( m_iOriginalXPos, m_iOriginalYPos, m_iOriginalWide, m_iOriginalTall );
 }
 
 //-----------------------------------------------------------------------------
@@ -160,27 +129,6 @@ void CHudHealth::VidInit()
 //-----------------------------------------------------------------------------
 void CHudHealth::OnThink()
 {
-        if ( m_iStyle != hud_healtharmor_style.GetInt() )
-        {
-                m_iStyle = hud_healtharmor_style.GetInt();
-
-                switch ( m_iStyle )
-                {
-                        case HUD_STYLE_DEFAULT:
-                                SetBounds( m_iOriginalXPos, m_iOriginalYPos, m_iOriginalWide, m_iOriginalTall );
-                                break;
-
-                        case HUD_STYLE_SIMPLE:
-                                SetBounds( simple_xpos, simple_ypos, simple_wide, simple_tall );
-                                break;
-
-                        case HUD_STYLE_LEGACY:
-                                SetBounds( legacy_xpos, legacy_ypos, legacy_wide, legacy_tall );
-                                break;
-                }
-        }
-
-
         int realHealth = 0;
         C_BasePlayer *local = C_BasePlayer::GetLocalPlayer();
         if ( local )
@@ -218,30 +166,11 @@ void CHudHealth::OnThink()
 
 void CHudHealth::Paint( void )
 {
-        // ============================================
-        // FIXED: استخدام نفس اللون للـ ProgressBar
-        // ============================================
         Color hudColor = GetHudColor();
         
         if( m_pHealthIcon )
         {
                 m_pHealthIcon->DrawSelf( icon_xpos, icon_ypos, icon_wide, icon_tall, hudColor );
-        }
-        
-        if ( hud_healtharmor_style.GetInt() == HUD_STYLE_DEFAULT )
-        {
-                // FIXED: رفع الـ ProgressBar قليلاً للأعلى
-                float adjustedYPos = progress_ypos - YRES(2); // يمكن تغيير القيمة من 2 إلى 3 أو 4 حسب الحاجة
-                
-                // draw the progress bar background
-                DrawBox( progress_xpos, adjustedYPos, progress_wide, progress_tall, m_ProgressBgColor, 1.0f );
-                
-                // FIXED: رسم الـ foreground بنفس لون الـ Health
-                if ( m_iHealth > 0 )
-                {
-                        // استخدام hudColor بدلاً من m_ProgressFgColor
-                        DrawBox( progress_xpos, adjustedYPos, progress_wide * Clamp( m_iHealth / 100.0f, 0.0f, 1.0f ), progress_tall, hudColor, 1.0f );
-                }
         }
 
         SetFgColor( hudColor );
--- game/client/cstrike/hud_armor.cpp
+++ game/client/cstrike/hud_armor.cpp
@@ -19,55 +19,28 @@
 #include "cs_gamerules.h"
 #include "hud_numericdisplay.h"
 #include "cs_hud_color.h"
-#include "cs_shareddefs.h"
-
-extern ConVar hud_healtharmor_style;
 
 class CHudArmor : public CHudElement, public CHudNumericDisplay
 {
 public:
-        DECLARE_CLASS_SIMPLE( CHudArmor, CHudNumericDisplay );
+	DECLARE_CLASS_SIMPLE( CHudArmor, CHudNumericDisplay );
 
-        CHudArmor( const char *name );
+	CHudArmor( const char *name );
 
-        virtual bool ShouldDraw();      
-        virtual void Paint();
-        virtual void Init();
-        virtual void OnThink();
-        virtual void ApplySchemeSettings( IScheme *scheme );
+	virtual bool ShouldDraw();	
+	virtual void Paint();
+	virtual void Init();
+	virtual void ApplySchemeSettings( IScheme *scheme );
 
 private:
-        CHudTexture *m_pArmorIcon;
-        CHudTexture *m_pArmor_HelmetIcon;
+	CHudTexture *m_pArmorIcon;
+	CHudTexture *m_pArmor_HelmetIcon;
 
-        CPanelAnimationVarAliasType( float, icon_xpos, "icon_xpos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, icon_ypos, "icon_ypos", "2", "proportional_float" );
+	CPanelAnimationVarAliasType( float, icon_xpos, "icon_xpos", "0", "proportional_float" );
+	CPanelAnimationVarAliasType( float, icon_ypos, "icon_ypos", "2", "proportional_float" );
 
-        CPanelAnimationVarAliasType( float, legacy_xpos, "legacy_xpos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, legacy_ypos, "legacy_ypos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, legacy_wide, "legacy_wide", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, legacy_tall, "legacy_tall", "0", "proportional_float" );
-
-        CPanelAnimationVarAliasType( float, simple_xpos, "simple_xpos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, simple_ypos, "simple_ypos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, simple_wide, "simple_wide", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, simple_tall, "simple_tall", "0", "proportional_float" );
-
-        CPanelAnimationVarAliasType( float, progress_xpos, "progress_xpos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, progress_ypos, "progress_ypos", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, progress_wide, "progress_wide", "0", "proportional_float" );
-        CPanelAnimationVarAliasType( float, progress_tall, "progress_tall", "0", "proportional_float" );
-        CPanelAnimationVar( Color, m_ProgressFgColor, "ProgressFgColor", "FgColor" );
-        CPanelAnimationVar( Color, m_ProgressBgColor, "ProgressBgColor", "BgColor" );
-
-        int m_iStyle;
-        int m_iOriginalXPos;
-        int m_iOriginalYPos;
-        int m_iOriginalWide;
-        int m_iOriginalTall;
-
-        float icon_wide;
-        float icon_tall;
+	float icon_wide;
+	float icon_tall;
 };
 
 
@@ -76,127 +49,79 @@ DECLARE_HUDELEMENT( CHudArmor );
 
 CHudArmor::CHudArmor( const char *pName ) : CHudNumericDisplay( NULL, "HudArmor" ), CHudElement( pName ), m_pArmorIcon( NULL )
 {
-        SetHiddenBits( HIDEHUD_HEALTH | HIDEHUD_PLAYERDEAD );
-        m_iStyle = -1;
-        m_iOriginalXPos = 0;
-        m_iOriginalYPos = 0;
-        m_iOriginalWide = 0;
-        m_iOriginalTall = 0;
+	SetHiddenBits( HIDEHUD_HEALTH | HIDEHUD_PLAYERDEAD );
 }
 
 
 void CHudArmor::Init()
 {
-        SetIndent(true);
+	SetIndent(true);
 }
 
 void CHudArmor::ApplySchemeSettings( IScheme *scheme )
 {
-        BaseClass::ApplySchemeSettings( scheme );
+	BaseClass::ApplySchemeSettings( scheme );
 
-        if( !m_pArmorIcon )
-        {
-                m_pArmorIcon = gHUD.GetIcon( "shield_bright" );
-        }
+	if( !m_pArmorIcon )
+	{
+		m_pArmorIcon = gHUD.GetIcon( "shield_bright" );
+	}
 
-        if( !m_pArmor_HelmetIcon )
-        {
-                m_pArmor_HelmetIcon = gHUD.GetIcon( "shield_kevlar_bright" );
-        }       
+	if( !m_pArmor_HelmetIcon )
+	{
+		m_pArmor_HelmetIcon = gHUD.GetIcon( "shield_kevlar_bright" );
+	}	
 
-        if( m_pArmorIcon )
-        {
-                icon_tall = GetTall() - YRES(2);
-                float scale = icon_tall / (float)m_pArmorIcon->Height();
-                icon_wide = ( scale ) * (float)m_pArmorIcon->Width();
-        }
-        GetBounds( m_iOriginalXPos, m_iOriginalYPos, m_iOriginalWide, m_iOriginalTall );
+	if( m_pArmorIcon )
+	{
+		icon_tall = GetTall() - YRES(2);
+		float scale = icon_tall / (float)m_pArmorIcon->Height();
+		icon_wide = ( scale ) * (float)m_pArmorIcon->Width();
+	}
 }
 
 
 bool CHudArmor::ShouldDraw()
 {
-        C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
-        if ( pPlayer )
-        {
-                return !pPlayer->IsObserver();
-        }
-        else
-        {
-                return false;
-        }
+	C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
+	if ( pPlayer )
+	{
+		return !pPlayer->IsObserver();
+	}
+	else
+	{
+		return false;
+	}
 }
 
 
 void CHudArmor::Paint()
 {
-        // ============================================
-        // FIXED: استخدام نفس اللون للـ ProgressBar
-        // ============================================
-        Color hudColor = GetHudColor();
-        
-        // Update the time.
-        C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
-        if ( pPlayer )
-        {
-                int iArmorValue = pPlayer->ArmorValue();
-                if( pPlayer->HasHelmet() && iArmorValue > 0 )
-                {
-                        if( m_pArmor_HelmetIcon )
-                        {
-                                m_pArmor_HelmetIcon->DrawSelf( icon_xpos, icon_ypos, icon_wide, icon_tall, hudColor );
-                        }
-                }
-                else
-                {
-                        if( m_pArmorIcon )
-                        {
-                                m_pArmorIcon->DrawSelf( icon_xpos, icon_ypos, icon_wide, icon_tall, hudColor );
-                        }
-                }
-
-                if ( hud_healtharmor_style.GetInt() == HUD_STYLE_DEFAULT )
-                {
-                        // FIXED: رفع الـ ProgressBar قليلاً للأعلى
-                        float adjustedYPos = progress_ypos - YRES(2); // يمكن تغيير القيمة من 2 إلى 3 أو 4 حسب الحاجة
-                        
-                        // draw the progress bar background
-                        DrawBox( progress_xpos, adjustedYPos, progress_wide, progress_tall, m_ProgressBgColor, 1.0f );
-                        
-                        // FIXED: رسم الـ foreground بنفس لون الـ Armor
-                        if ( iArmorValue > 0 )
-                        {
-                                // استخدام hudColor بدلاً من m_ProgressFgColor
-                                DrawBox( progress_xpos, adjustedYPos, progress_wide * Clamp( iArmorValue / 100.0f, 0.0f, 1.0f ), progress_tall, hudColor, 1.0f );
-                        }
-                }
-                
-                SetFgColor( hudColor );
-                SetDisplayValue( iArmorValue );
-                SetShouldDisplayValue( true );
-                BaseClass::Paint();
-        }
+	Color hudColor = GetHudColor();
+	
+	// Update the time.
+	C_CSPlayer *pPlayer = C_CSPlayer::GetLocalCSPlayer();
+	if ( pPlayer )
+	{
+		if( pPlayer->HasHelmet() && (int)pPlayer->ArmorValue() > 0 )
+		{
+			if( m_pArmor_HelmetIcon )
+			{
+				m_pArmor_HelmetIcon->DrawSelf( icon_xpos, icon_ypos, icon_wide, icon_tall, hudColor );
+			}
+		}
+		else
+		{
+			if( m_pArmorIcon )
+			{
+				m_pArmorIcon->DrawSelf( icon_xpos, icon_ypos, icon_wide, icon_tall, hudColor );
+			}
+		}
+		
+		SetFgColor( hudColor );
+		SetDisplayValue( (int)pPlayer->ArmorValue() );
+		SetShouldDisplayValue( true );
+		BaseClass::Paint();
+	}
 }
 
-void CHudArmor::OnThink()
-{
-        if ( m_iStyle != hud_healtharmor_style.GetInt() )
-        {
-                m_iStyle = hud_healtharmor_style.GetInt();
-
-                switch ( m_iStyle )
-                {
-                        case HUD_STYLE_DEFAULT:
-                                SetBounds( m_iOriginalXPos, m_iOriginalYPos, m_iOriginalWide, m_iOriginalTall );
-                                break;
-
-                        case HUD_STYLE_SIMPLE:
-                                SetBounds( simple_xpos, simple_ypos, simple_wide, simple_tall );
-                                break;
-
-                        case HUD_STYLE_LEGACY:
-                                SetBounds( legacy_xpos, legacy_ypos, legacy_wide, legacy_tall );
-                                break;
-                }
-        }
-}
